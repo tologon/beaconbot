3fb67a629ee05f8caa570390b7b0b65fa4c1d18f
jnicholas1 <pi@pi>
2016-07-18 15:59:14 +0000

    Refactor twdd_controller.py file.

--- twdd_controller.py
+++ twdd_controller.py
@@ -1,32 +1,31 @@
+from numpy import cos, sin
+
 # A simple two-wheel differential drive controller
-
 class TWDDController(object):
-    def __init__(self, b):
-        """Initialize the controller with the inter-wheel distance, b"""
-	self.b = b
+    def __init__(self, wheel_distance):
+        """Initialize the controller with the inter-wheel distance"""
+        self.wheel_distance = wheel_distance
 
-    def base_velocity(self, vl, vr):
-        return 0.5*(vl+vr)
+    def base_velocity(self, left_velocity, right_velocity):
+        return 0.5 * (left_velocity + right_velocity)
 
-    def ang_velocity(self, vl, vr):
-        return 1.0/self.b*(vr-vl)
+    def ang_velocity(self, left_velocity, right_velocity):
+        return 1.0 / self.b * (right_velocity - left_velocity)
 
-    def dead_reckon(self, vl, vr, t):
+    def dead_reckon(self, left_velocity, right_velocity, time):
         """Returns the (x, y) of the platform given the wheel velocities
-	    and the time step."""
-	from numpy import cos, sin
-	# assumes orientation along x axis
+        and the time step."""
+        # assumes orientation along x axis
 
-	# Ignore one wheel if the percent difference is low
-	if vl+vr < 0.01 or abs(2.0*(vl-vr)/(vl+vr)) < 0.01:
-	    return ( (t*vl, 0), 0)
+        # ignore one wheel if the percent difference is low
+        if (left_velocity + right_velocity < 0.01 or
+            abs(2.0 * (left_velocity - right_velocity) / (left_velocity + right_velocity)) < 0.01):
+            return ( (time * left_velocity, 0), 0)
 
-	theta = t/self.b*(vr-vl)
-	radius = self.b/2.0*((vl+vr)/(vr-vl))
+        theta = time / self.wheel_distance * (right_velocity - left_velocity)
+        radius = self.wheel_distance / 2.0 * ((left_velocity + right_velocity) / (right_velocity - left_velocity))
 
-	x = cos(theta/2.0)*(2.0*radius*sin(theta/2.0))
-	y = sin(theta/2.0)*(2.0*radius*sin(theta/2.0))
+        x = cos(theta / 2.0) * (2.0 * radius * sin(theta / 2.0))
+        y = sin(theta / 2.0) * (2.0 * radius * sin(theta / 2.0))
 
- 
- 	return ((x, y), theta)
-
+        return ((x, y), theta)
